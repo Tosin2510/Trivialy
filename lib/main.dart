f4a42892bd6d3df4b3590678a567fae83bee6061
Tosin Folarin <folarintosin2007@gmail.com>
2026-07-18 17:01:25 +0100

    Initialize firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:trivialy/home_screen.dart';
+import 'package:trivialy/auth_gate.dart';
+import 'package:trivialy/firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);  
   runApp(const MyApp());
 }
 
@@ -16,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         ),
-        home: const HomeScreen(),
+        home: AuthGate(),
     );
   }
 }
